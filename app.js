f4f8668ee30a402a8ff263ec4b7834be452a7879
Jason Lally <jason.lally@gmail.com>
2013-09-06 02:17:30 -0700

    initialize files for program

---
+++ app.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+
+	var show_application = function(){
+		$("#information").hide();
+        $(".header").hide();
+		$("#application").show();
+		window.scrollTo(0,500);
+	}
+
+    $(".navbar").sticky({topSpacing:0});
+
+	$("#application").hide(); 
+
+	$(".Apply .button").on( 'click', function() {
+		show_application();
+		window.location.hash = '#application';
+		
+		 /*
+		//Firefox for some reason does not refresh the DOM in iFrame
+	    if($.browser.mozilla){
+			document.getElementsByTagName('iframe')[0].src=document.getElementsByTagName('iframe')[0].src
+	    }    */
+
+	});
+
+
+
+
+
+	$("div#application a.back").click(function() {
+		$("#application").hide();
+		$("#information").show();
+        $(".header").show();
+		window.location.hash = '';
+	});
+		
+	$('.advisorimages ul li img').popover({
+		placement: 'bottom',
+		animation: false}
+	);
+		
+	// anchor scrolling
+	var didScroll = true;
+
+	var sections = $(".wrapper").each(function(index) {
+		$(this).data("height", $(this).height());
+	});
+	var links = $(".navbar a");
+
+	$(".navbar").localScroll({
+		axis : "y",
+		duration : 1000,
+		easing : "easeInOutExpo",
+		hash : true,
+        offset: -70
+	});
+		
+		
+	if(window.location.hash == '#application'){
+		show_application();
+	}
+	
+		
+
+
+});;
